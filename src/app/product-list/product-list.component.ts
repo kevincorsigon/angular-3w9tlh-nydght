f42f213c6fb9840fbcecb191a8b9f5d2d793e553
kevincorsigon <kevincorsigon@hotmail.com>
2020-04-11 14:27:35 -0300

    7 files modified, 10 added

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { Input } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
 import { products } from '../products';
 
 @Component({
@@ -8,10 +9,15 @@ import { products } from '../products';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
+
   products = products;
 
   share() {
     window.alert('The product has been shared!');
+  }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
   }
 }
 
